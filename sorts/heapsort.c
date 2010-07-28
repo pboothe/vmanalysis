829ce7a1af6583b0e8041286873d685e5745af2e
Peter <peter@emulated.(none)>
2010-07-27 23:55:56 -0400

    Heapsort back to working and the Makefile is simplified

--- sorts/heapsort.c
+++ sorts/heapsort.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/resource.h>
+#include <bsd/stdlib.h>
 
 int cmp(const void *i, const void *j)
 {
@@ -14,28 +14,16 @@ int main(int argc, char **argv)
 {
         int *array, i, repeats;
         long SIZE;
-        struct rlimit limits;
-        SIZE = atol(argv[1])/sizeof(int)*Mb;
+        SIZE = atol(argv[1])*Mb/sizeof(int);
+	printf("Allocated %ld ints\n", SIZE);
 
         repeats = atoi(argv[2]);
 
-        limits.rlim_cur = SIZE*sizeof(int)/2;
-        limits.rlim_max = SIZE*sizeof(int)/2;
-        if (setrlimit(RLIMIT_RSS, &limits)) {
-            perror("setting limits");
-            exit(1);
-        }
-
-        array = malloc(SIZE);
-        if (NULL == array) {
-            perror("allocating");
-            exit(1);
-        }
+        array = malloc(SIZE*sizeof(int));
         fprintf(stderr, "%p\n", array);
         
         while (repeats--> 0) { 
                 for (i = 0; i < SIZE; i++) {
-                    fprintf(stderr, "%d\n", i);
                     array[i] = i*7 % SIZE;
                 }
 
